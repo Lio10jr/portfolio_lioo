5ccdc48c1ebfb31e4f8051ccf9e08c765cd76c37
Lio10jr <kevinzambrano593@gmail.com>
2024-09-22 14:08:32 -0500

    Firt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolBarComponent } from './shared/components/tool-bar/tool-bar.component';
+
+// Angular Material 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/tool-bar/tool-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-bar',
+  templateUrl: './tool-bar.component.html',
+  styleUrls: ['./tool-bar.component.css']
+})
+export class ToolBarComponent {
+
+  public menuList = [
+    { title: 'Home', path: '/home' },
+    { title: 'Projects', path: '/projects' }
+  ]
+}
